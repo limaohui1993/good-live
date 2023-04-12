88a2f8f09a2e1a13d5f060f28171a94bfa49d07b
limaohui <limaohui1993@outlook.com>
2023-04-12 22:11:04 +0800

    登陆reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux"
 import city from "./city"
 import search from "./search"
+import login from "./login"
 
 const rootReducer=combineReducers({
     city,
-    search
+    search,
+    login
 })
 
 export default rootReducer
